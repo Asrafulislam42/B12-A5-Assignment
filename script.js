98b7a493a748c7fcb0c39aa3e7a80bdd5cbd2f3c
Asrafulislam42 <islamasraful3042@gmail.com>
2025-08-29 16:28:13 +0600

    add javascript function

--- script.js
+++ script.js
@@ -20,8 +20,10 @@
 //   btn.addEventListener("click", updateHeartCount);
 // });
 
+// Heart button click
 let hearts = 0;
 const heartButtons = document.querySelectorAll(".heart-btn");
+const heartCount = document.getElementById("heartCount");
 
 for (let i = 0; i < heartButtons.length; i++) {
   heartButtons[i].addEventListener("click", function () {
@@ -53,17 +55,18 @@ for (let i = 0; i < heartButtons.length; i++) {
 //   });
 // });
 
-var coins = 100;
-var callButtons = document.querySelectorAll(".call-btn");
-var coinCount = document.getElementById("coin-count");
-var historyList = document.getElementById("history-list");
+// Call button click
+let coins = 100;
+const callButtons = document.querySelectorAll(".call-btn");
+const coinCount = document.getElementById("coinCount");
+const historyList = document.getElementById("historyList");
 
 function makeCall() {
-  var serviceName = this.getAttribute("data-name");
-  var serviceNumber = this.getAttribute("data-number");
+  let serviceName = this.getAttribute("data-name");
+  let serviceNumber = this.getAttribute("data-number");
 
   if (coins < 20) {
-    alert("Not enough coins to make a call!");
+    alert("You don't have enough coins to make this call");
     return;
   }
 
@@ -71,11 +74,13 @@ function makeCall() {
   coinCount.textContent = coins;
   alert("Calling " + serviceName + " at " + serviceNumber);
 
-  var li = document.createElement("li");
+  let li = document.createElement("li");
   li.textContent = serviceName + " - " + serviceNumber;
   historyList.appendChild(li);
 }
 
-for (var i = 0; i < callButtons.length; i++) {
+for (let i = 0; i < callButtons.length; i++) {
   callButtons[i].addEventListener("click", makeCall);
 }
+
+// Copy button click
